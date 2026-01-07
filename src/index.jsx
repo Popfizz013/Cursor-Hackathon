96b3f3a2eac5558290c75fcdc323e612174b3da7
Liam Degand <degandliam013@gmail.com>
2026-01-07 13:34:53 -0800

    Refactored remaining files in the src folder

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-// Hide loading screen once React is ready
 const hideLoading = () => {
-  const loading = document.getElementById('loading');
-  if (loading) {
-    loading.classList.add('hidden');
-    setTimeout(() => {
-      loading.remove();
-    }, 500);
-  }
+	const loading = document.getElementById('loading');
+	if (loading) {
+		loading.classList.add('hidden');
+		setTimeout(() => {
+			loading.remove();
+		}, 500);
+	}
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
